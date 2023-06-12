e47046aea1dd07828482e4519e1521b47b0cd8d1
Kiran Itagi <34514599+KiranVItagi@users.noreply.github.com>
2023-06-12 21:06:23 +0530

    Update and rename cleanup.sh to list.sh

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-total=`ls /home/ubuntu/clean | wc -l`
-oldfiles=`expr $total - 50`
-if [ $oldfiles -gt 0 ]
-then
-ls -rt | head -$oldfiles | xargs rm -rf
-fi
---
+++ list.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo " the no of files and directories are `ls -l` "
