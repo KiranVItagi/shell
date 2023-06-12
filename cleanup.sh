fb8924430e69ae801d4fddd2153a0479e225ee04
Kiran Itagi <34514599+KiranVItagi@users.noreply.github.com>
2023-06-12 20:49:51 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-total=`ls -l | wc -l`
-oldfiles=`expr $total - 2`
+total=`ls /home/ubuntu/shell/cleanup | wc -l`
+oldfiles=`expr $total - 50`
 if [ $oldfiles -gt 0 ]
 then
 ls -rt | head -$oldfiles | xargs rm -rf
