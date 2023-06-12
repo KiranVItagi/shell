4ce18c31bb7cd96dae9d556b99e3ce20374a50e1
Kiran Itagi <34514599+KiranVItagi@users.noreply.github.com>
2023-06-12 21:25:56 +0530

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-echo " the no of files and directories are `ls -l` "
+#!/bin/bash 
+total=`ls /root/dir1/shell | wc -l`
+echo "Total = $total"
+old_files=`expr $total - 50`
+echo "Old files = $old_files"
+if [ $old_files -gt 0 ]
+then
+ls -rt | head -$old_files | xargs rm -rf
+fi
