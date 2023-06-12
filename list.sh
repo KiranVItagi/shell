4c1bab9ffd8cebf0ed3e1c67e504dce0527caa97
Kiran Itagi <34514599+KiranVItagi@users.noreply.github.com>
2023-06-12 21:26:24 +0530

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 total=`ls /root/dir1/shell | wc -l`
 echo "Total = $total"
-old_files=`expr $total - 50`
+old_files=`expr $total - 90`
 echo "Old files = $old_files"
 if [ $old_files -gt 0 ]
 then
